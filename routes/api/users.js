f23c8617e36688711758984e4eb4bede8f6485fa
EddieH <ehajek@outlook.com>
2021-10-03 14:34:21 -0400

    fixing route api user

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,14 +1,14 @@
 const router = require('express').Router();
 
 const {
-    //get from controller file - need to create 
-    getAllUser,
-    getUserById,
-    createUser,
-    updateUser,
-    deleteUser,
-    createFriend,
-    removeFriend
+  //get from controller file - need to create 
+  getAllUser,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+  createFriend,
+  deteleFriend
 } = require('../../controllers/api/Users')
 
 
@@ -17,12 +17,16 @@ router
   .get(getAllUser)
   .post(createUser);
 
+router
+  .route(':id')
+  .get(getUserById)
+  .put(updateUser)
+  .delete(deleteUser);
 
-  router
-    .route(':id')
-    .get(getUserById)
-    .put(updateUser)
-    .delete(deleteUser);
+router
+  .route('/:id/friends/:friendId')
+  .post(createFriend)
+  .delete(deleteFriend);
 
 
 module.exports = router;
